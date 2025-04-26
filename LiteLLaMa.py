8232f7265d81a8279911394b8cb096fe6b9a706b
Rey <164343513+Reyisreal@users.noreply.github.com>
2025-04-26 14:13:20 -0700

    Your descriptive commit message

---
+++ LiteLLaMa.py
@@ -0,0 +1,11 @@
+from litellm import completion
+
+response = completion(
+    # enter the desired model name here, e.g., "llama2-7b-chat" or "llama2-13b-chat"
+    # model="ollama/[MODEL_NAME]", 
+    
+    #enter the desired prompt here, e.g., "What is the capital of France?"
+    # messages=[{ "content": "[prompt]","role": "user"}], 
+    api_base="http://localhost:11434"
+)
+print(response["choices"][0]["message"]["content"])
